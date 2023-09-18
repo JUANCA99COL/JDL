0482a76ab8117df54bca1862757e39dbc081d69d
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2023-09-18 21:29:39 +1200

    dark mode and removing icons

--- js/script.js
+++ js/script.js
@@ -43,7 +43,6 @@ var imgArray = [
 
 
     // SIDE NAV JS 
-
     function openNav() {
       document.getElementById("mySidenav").style.width = "250px";
     }
@@ -51,3 +50,58 @@ var imgArray = [
     function closeNav() {
       document.getElementById("mySidenav").style.width = "0";
     }
+
+    // darkmode 
+    const toggleSwitch = document.querySelector('input[type="checkbox"]');
+    const nav = document.getElementById('nav');
+    const toggleIcon = document.getElementById('toggle-icon');
+    const menuBtn = document.getElementById('menu-btn');
+    const arrow = document.getElementById('arrow');
+    const arrow2 = document.getElementById('arrow2');
+    const disMenu1 = document.getElementById('dis-menu1');
+    const disMenu2 = document.getElementById('dis-menu2');
+    const disMenu3= document.getElementById('dis-menu3');
+    const disMenu4 = document.getElementById('dis-menu4');
+    // const disMenu = document.getElementsByClassName('dis-menu');
+
+   // Dark mode
+    function darkMode() {
+      nav.style.backgroundColor = 'black';
+      menuBtn.style.color = 'white';
+      arrow.style.color = 'white';
+      arrow2.style.color = 'white';
+      disMenu1.style.color = 'white';
+      disMenu2.style.color = 'white';
+      disMenu3.style.color = 'white';
+      disMenu4.style.color = 'white';
+      toggleIcon.children[0].classList.remove('fa-sun');
+      toggleIcon.children[0].classList.add('fa-moon');
+    }
+
+     // light mode
+     function lightMode() {
+      nav.style.backgroundColor = 'white';
+      menuBtn.style.color = 'black';
+      arrow.style.color = 'black';
+      arrow2.style.color = 'black';
+      disMenu1.style.color = 'black';
+      disMenu2.style.color = 'black';
+      disMenu3.style.color = 'black';
+      disMenu4.style.color = 'black';
+      toggleIcon.children[0].classList.remove('fa-moon');
+      toggleIcon.children[0].classList.add('fa-sun'); 
+    }
+
+    // switch theme dynamically
+    function switchTheme(event){
+      if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+      }
+    }
+
+    //Event Listener
+    toggleSwitch.addEventListener('change', switchTheme);
